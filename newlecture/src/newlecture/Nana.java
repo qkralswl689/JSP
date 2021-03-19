3e2d7854f8f0bde07573f40892b4025b2b41f418
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-03-19 16:40:39 +0900

    03.19

--- newlecture/src/newlecture/Nana.java
+++ newlecture/src/newlecture/Nana.java
@@ -18,12 +18,26 @@ public class Nana extends HttpServlet {
 		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = resp.getWriter();
 		
-		for(int i=0; i<100; i++) {
-			out.println((i+1) + ": 안녕 Servlet!!<br >");
+		// 입력값이 입력 됐을 경우
+		// cnt : 입력값 -> 무조건 문자열로 전달된다  => Integer로 형변환
+//		int cnt = Integer.parseInt(req.getParameter("cnt"));
+//		
+//		for(int i=0; i<cnt; i++) {
+//			out.println((i+1) + ": 안녕 Servlet!!<br >");
+		
+		// 입력값이 입력 안 됐을 경우
+		
+		String cnt_ = req.getParameter("cnt"); // 임시변수 설정
+		
+		int cnt = 10; // 기본값 설정
+		if(cnt_ != null && !cnt_.equals("")) { // cnt_의 값이  null이 아니거나 빈문자열이 아닐경우
+			cnt = Integer.parseInt(cnt_); // cnt_의 값을 cnt(기본값)으로 저장한다
 		}
 		
+		for(int i=0; i<cnt; i++) {
+			out.println((i+1) + ": 안녕 Servlet!!<br >");
+		}
 	
 	}
-	
-
 }
+
